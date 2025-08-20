0d90e830fa807efdf1d9b40c8a647055609794c4
Kuba <kuba@ipor.io>
2025-08-20 21:05:33 +0200

    Turn progress.tsx into CSS Modules

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
+import styles from "./progress.module.css"
 
 function Progress({
   className,
@@ -11,15 +12,12 @@ function Progress({
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
-      className={cn(
-        "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
-        className
-      )}
+      className={cn(styles.root, className)}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
+        className={styles.indicator}
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
