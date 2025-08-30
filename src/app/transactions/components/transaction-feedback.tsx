65a6c3f2903f5bc0e084c22f4a7ffb7f3ec9eb63
Kuba <kuba@ipor.io>
2025-08-30 15:31:34 +0200

    Merge transaction state components into transaction-feedback

--- src/app/transactions/components/transaction-feedback.tsx
+++ src/app/transactions/components/transaction-feedback.tsx
@@ -1,7 +1,10 @@
 import type { TransactionState } from '@/app/transactions/hooks/use-transaction-state';
-import { TransactionSuccess } from './transaction-success';
-import { TransactionError } from './transaction-error';
-import { TransactionPending } from './transaction-pending';
+import { TransactionLink } from '@/components/ExternalLink/transaction-link';
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { CheckIcon, Loader2Icon } from 'lucide-react';
+import { AlertCircleIcon, XIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 
 interface Props {
   transactionState: TransactionState;
@@ -10,11 +13,76 @@ interface Props {
 export const TransactionFeedback = ({
   transactionState,
 }: Props) => {
-  return (
-    <div>
-      <TransactionError transactionState={transactionState} />
-      <TransactionPending transactionState={transactionState} />
-      <TransactionSuccess transactionState={transactionState} />
-    </div>
-  );
+  const { txStatus, resetTransactionState } = transactionState;
+
+  if (txStatus.type === 'error') {
+    const { error } = txStatus;
+
+    return (
+      <Alert>
+        <AlertCircleIcon />
+        <AlertTitle>
+          Can't perform this transaction
+        </AlertTitle>
+        {Boolean(error) && (
+          <AlertDescription className="pt-4">
+            <ScrollArea className="w-full overflow-auto">
+              <pre>
+                {getErrorLog(error)}
+              </pre>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+            <Button
+              onClick={resetTransactionState}
+              variant="outline"
+              size="sm"
+              className="mt-4"
+            >
+              <XIcon />
+              Dismiss
+            </Button>
+          </AlertDescription>
+        )}
+      </Alert>
+    );
+  };
+
+  if (txStatus.type === 'pending') {
+    return (
+      <Alert>
+        <Loader2Icon className="animate-spin" />
+        <AlertTitle>
+          <span>Transaction pending</span>
+        </AlertTitle>
+        <AlertDescription>
+          <p>Preview in block explorer: <TransactionLink hash={txStatus.hash} /></p>
+        </AlertDescription>
+      </Alert>
+    );
+  };
+
+  if (txStatus.type === 'success') {
+    return (
+      <Alert>
+        <CheckIcon className="text-green-800!" />
+        <AlertTitle>
+          Transaction successful
+        </AlertTitle>
+        <AlertDescription>
+          <p>Preview in block explorer: <TransactionLink hash={txStatus.hash} /></p>
+        </AlertDescription>
+      </Alert>
+    );
+  };
+
+  return null;
+};
+
+const getErrorLog = (originalError: unknown) => {
+  const originalErrorMsg =
+    originalError instanceof Error
+      ? originalError.message
+      : JSON.stringify(originalError);
+
+  return originalErrorMsg;
 };
